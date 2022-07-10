f58270b5f4674d4c38a240783167224b86e05106
andressa05gomes <andressa05gomes@gmail.com>
2022-07-10 16:51:38 -0300

    projeto

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import ApplicationRoutes from "./Routes/ApplicationRoutes";
+
+function App() {
+    return (
+        <ApplicationRoutes />
+    )
+}
+
+export default App
---
+++ src/pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,12 @@
+import Header from '../../Header/Header'
+import image from '../../assets/foto02.svg'
+
+function Portifolio() {
+    return (
+        <Header image={image} descricao="Mulher com  site">
+            Meus Projetos
+        </Header>
+    )
+}
+
+export default Portifolio
